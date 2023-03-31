64d45f04b25606d079441f726cbb50ffd5c2f568
koyaaoyama <aoyama.ko0920@gmail.com>
2023-03-31 09:57:15 -0700

    implement solution to Linked List Cycle

--- 141LinkedListCycle.ts
+++ 141LinkedListCycle.ts
@@ -1,1 +1,24 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+}
 
+const hasCycle = (head: ListNode | null): boolean => {
+  if (!head || !head.next) return false
+
+  let slow: ListNode | null | undefined = head
+  let fast: ListNode | null = head
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow?.next
+    fast = fast.next.next
+
+    if (slow === fast) return true
+  }
+
+  return false
+}
